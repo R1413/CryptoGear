1f9a021fc9212455e87abb56d2ba32a0be5b5df3
José Cañizares <jose.canizares@colorado.edu>
2016-02-18 09:00:06 -0700

    Put a text field and some text and referenced them in MainActivity.java

--- CryptoGear/app/src/main/java/com/jocundstudio/cryptogear/MainActivity.java
+++ CryptoGear/app/src/main/java/com/jocundstudio/cryptogear/MainActivity.java
@@ -10,43 +10,40 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView Output;
     EditText EnterMessage;
-    EditText Output;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        Output = (TextView) findViewById(R.id.Output);
+        EnterMessage = (EditText) findViewById(R.id.EnterMessage);
 
+        Button EncryptButton = (Button) findViewById(R.id.EncryptButton);
+        //set OnClick Event on the Encrypt Button
+        EncryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String Message = EnterMessage.getText().toString();
+                String Answer = "Ok. Cool.";
+
+                Output.setText(Answer);
+
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
+
+
 }
