3512f80170698b2395c5df97ab18c30b96092da3
José Cañizares <jose.canizares@colorado.edu>
2016-02-26 18:15:26 -0700

    Added hide keyboard when user clicks away or when he presses enter.

--- CryptoGear/app/src/main/java/com/jocundstudio/cryptogear/MainActivity.java
+++ CryptoGear/app/src/main/java/com/jocundstudio/cryptogear/MainActivity.java
@@ -16,11 +16,21 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.util.Log;
+import android.widget.TextView.OnEditorActionListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.view.KeyEvent;
+import android.content.Context;
+import android.app.Activity;
+import android.view.ViewGroup;
+import android.view.MotionEvent;
+import android.view.View.OnTouchListener;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView Output;
-    EditText Message;
+    EditText UserName;
+    EditText Password;
     SeekBar adjustFontSize;
 
     float font_size;
@@ -33,12 +43,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+
+
+
+
+
+        //Hide the keyboard when the enter key is pressed.
+        //in XML file
+        //android:imeOptions="actionDone"
+        //android:singleLine="true"
+        //on the EditTexts
+
+
+
+        //This will hide keyboard when user clicks outside of an EditText
+        //Loops through all EditTexts so that they all have this property
+        setupUI(findViewById(R.id.parent));
+
+
+
+
+
+
+
+
         //Reference the text field and text view
         Output = (TextView) findViewById(R.id.Output);
-        Message = (EditText) findViewById(R.id.EnterMessage);
+        UserName = (EditText) findViewById(R.id.EnterUserName);
+
+        Password = (EditText) findViewById(R.id.EnterPassword);
 
         //reference the seekbar
         adjustFontSize = (SeekBar) findViewById(R.id.adjustFontSize);
+
+
+
+
+
+
 
 
 
@@ -49,14 +94,14 @@ public class MainActivity extends AppCompatActivity {
         text_info = sharedPreferences.getString(getString(R.string.TEXT_INFO), "");
 
         //set the text
-        Message.setText(text_info);
+        UserName.setText(text_info);
 
 
 
         font_size = sharedPreferences.getFloat(getString(R.string.FONT_SIZE), 25);
 
         //set the text size
-        Message.setTextSize(TypedValue.COMPLEX_UNIT_PX, font_size);
+        UserName.setTextSize(TypedValue.COMPLEX_UNIT_PX, font_size);
 
         if (font_size == 25) {
 
@@ -73,7 +118,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        //set the font color
+
+
+
+
+
+
 
 
 
@@ -87,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar adjustFontSize, int progress, boolean fromUser) {
 
                 //Change text size depending on the progress of the seekbar
-                Message.setTextSize(TypedValue.COMPLEX_UNIT_PX, progress);
+                UserName.setTextSize(TypedValue.COMPLEX_UNIT_PX, progress);
 
             }
 
@@ -101,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar adjustFontSize) {
 
-                font_size = Message.getTextSize();
+                font_size = UserName.getTextSize();
 
 
             }
@@ -115,13 +165,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String message = Message.getText().toString();
+                String message = UserName.getText().toString();
                 String Answer = "Ok. Cool.";
 
                 Output.setText(Answer);
 
             }
         });
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
@@ -140,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
 
         editor.putFloat(getString(R.string.FONT_SIZE), font_size);
 
-        editor.putString(getString(R.string.TEXT_INFO), Message.getText().toString());
+        editor.putString(getString(R.string.TEXT_INFO), UserName.getText().toString());
 
         //save all this information in the sharedPreferences
         editor.commit();
@@ -164,6 +225,54 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+    //hideKeyboard function
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+
+
+
+    //function for hiding keyboard when user clicks away from EditText
+    public void setupUI(View view) {
+
+        //Set up touch listener for non-text box views to hide keyboard.
+        if(!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(MainActivity.this);
+                    return false;
+                }
+
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                setupUI(innerView);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 
 
 
