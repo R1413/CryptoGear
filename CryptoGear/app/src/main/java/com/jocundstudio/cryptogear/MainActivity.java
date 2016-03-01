36a7813efed22431b5b337ef66d25f76038671cf
José Cañizares <jose.canizares@colorado.edu>
2016-02-29 19:10:06 -0700

    edited AndroidManifest.xml

--- CryptoGear/app/src/main/java/com/jocundstudio/cryptogear/MainActivity.java
+++ CryptoGear/app/src/main/java/com/jocundstudio/cryptogear/MainActivity.java
@@ -1,30 +1,20 @@
 package com.jocundstudio.cryptogear;
 
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
-import android.util.Log;
-import android.widget.TextView.OnEditorActionListener;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.view.KeyEvent;
-import android.content.Context;
 import android.app.Activity;
 import android.view.ViewGroup;
 import android.view.MotionEvent;
 import android.view.View.OnTouchListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     EditText UserName;
     EditText Password;
     SeekBar adjustFontSize;
+
+
+
+
+    String loginName;
+    String loginPassword;
 
     float font_size;
 
@@ -159,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Button EncryptButton = (Button) findViewById(R.id.EncryptButton);
+        Button EncryptButton = (Button) findViewById(R.id.Login);
         //set OnClick Event on the Encrypt Button
         EncryptButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -266,8 +262,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //method called when user presses login
+    public void userLogin(View view) {
+
+            //save the username to the string loginName
+            loginName = UserName.getText().toString();
+            loginPassword = Password.getText().toString();
+
+            String method = "login";
+
+            //new BackgroundTask instance
+            //Async task                                    //pass in context
+            BackgroundTask backgroundTask = new BackgroundTask(this);
+
+            backgroundTask.execute(method, loginName, loginPassword);
 
 
+
+
+
+
+
+
+
+
+
+    }
 
 
 
