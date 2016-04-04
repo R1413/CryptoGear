6b2c78fd6777301266f23310beacf6a87f3f25a6
José Cañizares <jose.canizares@colorado.edu>
2016-04-04 16:59:42 -0600

    Added website files for login.

--- TESTING.md
+++ TESTING.md
@@ -1,5 +1,19 @@
-Who: José Cañizares, Anh-Khoa Than, Sayed Sarder, Raymond Duncan
-Title: Crypto
-Vision: To help more people understand how encryption works.
-Automated Tests:
-User Acceptance Tests:
+####Who:
+José Cañizares, Anh-Khoa Than, Sayed Sarder, Raymond Duncan <br>
+####Title:
+Crypto <br>
+####Vision:
+To help more people understand how encryption works. <br>
+####Automated Tests:
+<br>
+####User Acceptance Tests:
+
+Test 1
+Make
+1.
+2.
+3.
+
+Expected Results
+
+<br>
---
+++ Website/login.js
@@ -0,0 +1,23 @@
+
+
+
+$(document).ready(function() {
+// Start when document will be ready.
+$("#LogIn").click(function() {
+
+//$("#passwordClone0").removeClass("invalid");
+
+var email = $("#email").val(); // Store E-mail input value in the variable email.
+var password = $("#password").val(); // Store Password input value in the variable password.
+/* Check if email=formget@gmail.com and password=fugo then,Show the message Account Validated!!! in the Div having id message.*/
+if (email == "formget@gmail.com" && password == "fugo") {
+// $("#message").html("Account Validated!!!");
+}
+/* If E-mail and Password do not match then shake Div having id maindiv and show the message "***Invalid email or password***" in the div having id message.*/
+else {
+$("#passwordClone0").effect("shake", { direction: "left", times: 3, distance: 8}, 400);
+//$("#passwordClone0").addClass("invalid");
+// $("#message").html('***Invalid email or password***');
+}
+});
+});
