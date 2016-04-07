9c0fe4c03d9aba93b853c4b208b116187f1b78a0
Raymond D <raydunc15@gmail.com>
2016-04-07 01:57:55 -0600

    Altered the login.html and login.js files to check for valid emails and to go to the index page upon entry of a vaild email and password combo

--- Website/login.js
+++ Website/login.js
@@ -5,19 +5,30 @@ $(document).ready(function() {
 $("#LogIn").click(function() {
     
     
-    window.location = 'index.html';
     console.log("clicked");
 
-    window.location.href="index.html";
-//$("#passwordClone0").removeClass("invalid");
-
+var validEntry = true;
 var email = $("#email").val(); // Store E-mail input value in the variable email.
 var password = $("#password").val(); // Store Password input value in the variable password.
-/* Check if email=formget@gmail.com and password=fugo then,Show the message Account Validated!!! in the Div having id message.*/
-if (email == "formget@gmail.com" && password == "fugo") {
-// $("#message").html("Account Validated!!!");
+if(!/^[A-Za-z0-9_\.\-]*@[A-Za-z]*\.[A-Za-z]*$/.test(email)) {
+    console.log("Invalid Email");
+    validEntry = false;
+
 }
 /* If E-mail and Password do not match then shake Div having id maindiv and show the message "***Invalid email or password***" in the div having id message.*/
+if(1 === 2){
+    console.log("Invalid Password");
+    validEntry = false;
+}
+if(validEntry){
+    if(email === "sample@colorado.edu" && password === "samplePassword"){
+        console.log("Valid Login");
+        window.location = 'index.html';
+    }
+    else{
+        console.log("Invslid Login");
+    }
+}
 else {
 $("#passwordClone0").effect("shake", { direction: "left", times: 3, distance: 8}, 400);
 //$("#passwordClone0").addClass("invalid");
@@ -26,11 +37,3 @@ $("#passwordClone0").effect("shake", { direction: "left", times: 3, distance: 8}
 });
 });
 
-
-function clickedButton()
-            {
-
-                window.location = 'index.html';
-
-            }
-            
