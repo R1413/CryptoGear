b0e2eecd2019f492373cdc51d560ae27baaa8f19
José Cañizares <jose.canizares@colorado.edu>
2016-04-12 23:42:51 -0600

    Switched to using PHP instead of HTML. Working on Logging in.

--- Android/User_Accounts.sql
+++ Android/User_Accounts.sql
@@ -16,3 +16,26 @@ INSERT INTO User_Accounts VALUE (DEFAULT, DEFAULT, 'anhkhoa', '1234', 'anhkhoa.t
 INSERT INTO User_Accounts VALUE (DEFAULT, DEFAULT, 'raymond', '1234', 'raymond.duncan@colorado.edu', 'Boulder', 'USA', NULL, '0', '0', '0', DEFAULT);
 INSERT INTO User_Accounts VALUE (DEFAULT, DEFAULT, 'joca', '1234', 'jose.canizares@colorado.edu', 'Boulder', 'USA', NULL, '0', '0', '0', DEFAULT);
 INSERT INTO User_Accounts VALUE (DEFAULT, DEFAULT, 'sayed', '1234', 'sayed.sarders@colorado.edu', 'Littleton', 'USA', NULL, '0', '0', '0', DEFAULT);
+
+CREATE TABLE Game_Messages(
+Ciphertext VARCHAR(140) NOT NULL,
+Plaintext VARCHAR(140) NOT NULL,
+Message_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Sender VARCHAR(60) NOT NULL,
+Recipient VARCHAR(60) NOT NULL,
+Viewed BOOLEAN NOT NULL DEFAULT 0,
+Completed BOOLEAN NOT NULL DEFAULT 0);
+
+
+INSERT INTO Game_Messages VALUE ("This is my encrypted message for Crypto, not Twitter.", "This is my encrypted message for Crypto, not Twitter.", NULL, 'jose.canizares@colorado.edu', 'raymond.duncan@colorado.edu', DEFAULT, DEFAULT);
+
+
+
+CREATE TABLE Friend_Table(
+Friend_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Requester VARCHAR(60) NOT NULL,
+Receiver VARCHAR(60) NOT NULL,
+isFriend BOOLEAN NOT NULL DEFAULT 0);
+
+
+INSERT INTO Friend_Table VALUE (DEFAULT, 'jose.canizares@colorado.edu', 'radu5022@colorado.edu', DEFAULT);
--- Website/login.php
+++ Website/login.php
@@ -1,94 +1,26 @@
 <?php
-$servername = "64.6.232.42";
-$username = "vivoapps";
-$password = "Jump2tinydoc";
-$dbname = "vivoapps_CryptoDatabase";
 
+/* 
+ * Vivo Applications
+ *
+ * PHP for the login page.
+ */
 
-/*
-if (!isset($_GET["email"])) {
-    print("email not valid.");
-}
+include("shared.php");
 
-if (!isset($_GET["password"])) {
-    print("password not valid.");
-}
-*/
-
-// else {
-
-
-    //$Email = $_GET["email"];
-    //$Password = $_GET["password"];
-
-    $Email = "jose.canizares@colorado.edu";
-    $Password = "1234";
-
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-//$sqlEmail =  $conn->quote($Email);
-//$sqlPassword = $conn->quote($Password);
-
-
-$sql = "SELECT * FROM User_Accounts";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    //while($row = $result->fetch_assoc()) {
-        
-        
-        //echo "id: " . $row["id"]. " - Name: " . $row["UserName"]. " " . $row["Password"]. "<br>";
-    //}
-    
-    ?>
-    
-    <h1>Results for <?= $first_name ?> <?= $last_name ?></h1>
-                            
-        <table>
-            <caption>All Users</caption>
-            <?= printTable($result) ?>
-        </table>
-        
-<?php
-} else {
-    echo "0 results";
-}
-
-
-$conn->close();
-
-// }
-
-
-
-
-function printTable($rows) {
-        $count = 1;
-        ?>
-        <tr><th>#</th><th>username</th><th>password</th></tr>
-        <?php
-        foreach($rows as $row) { 
-            ?>
-            <tr>
-                <td><?= $count ?></td>
-                <td><?=$row["UserName"]?></td>
-                <td><?=$row["Password"]?></td>
-            </tr>
-            <?php
-            $count++;
-        }
-    }
+$description = "Login to Crypto, adventure awaits.";
+$keywords = "crypto, login, vivo, applications";
+$cssfiles = ["styles/login.css"];
+$jsfiles = null;
+printHeader("Login", $description, $keywords, $cssfiles, $jsfiles);
 ?>
 
-
-
+<div id="login-content">
+    <img id="crypto-logo-login" src="images/crypto_logo_gray.svg" alt="Crypto" width="750px" heigth="auto" />
+    <p>Go ahead and log in. Crypto awaits you.</p>
+    <form id="loginform" action="login_utility.php" method="post">
+        <input autofocus="autofocus" id="email-input" name="email" placeholder="Email" size="30" type="text" />
+        <input autofocus="autofocus" id="password-input" name="password" placeholder="Password" size="30" type="text" />
+        <input id="login-button" type="submit" value="Log In" />
+    </form>
+</div>
