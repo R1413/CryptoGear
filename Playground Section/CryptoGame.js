4c012d72047c33726d6c6429d3213e5aecc99e25
ManUtd4Life <anhkhoa.than@colorado.edu>
2016-04-19 19:52:04 -0600

    Finished Playground

---
+++ Playground Section/CryptoGame.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+	console.log("document ready")
+	jQuery('.tabs .tab-list a').on('click', function(e){
+		var currentAttrValue = jQuery(this).attr('href');
+
+		//Show/Hide Tabs
+		jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+		console.log(currentAttrValue);
+
+
+		//Change/remove current tab to active
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
+});
